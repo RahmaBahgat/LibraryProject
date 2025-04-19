5a6fc95e9f23f4ef1cddf305aec374f4fc97c738
Doha-yasser <183983657+Doha-yasser@users.noreply.github.com>
2025-04-19 12:59:23 +0200

    Create help.js

---
+++ js/help.js
@@ -0,0 +1,54 @@
+function createAccount(){
+    alert(
+        `Create account \n
+        1.Click 'Sign Up' in the top-right corner
+        2.Fill in your details:
+            Enter your full name
+            Add your email address
+            secure password (8+ characters)
+            Check your email and click the confirmation link
+            Log in to access all features
+
+        `
+    );
+}
+
+function search() {
+    alert(
+        `U can use the search bar on the top and get the book using its title , author or category`
+    );
+}
+
+
+function borrowANDadd() {
+    alert(
+        `choose the book u want and press into borrow button or favourite `
+    );
+}
+
+function aboutAuthor() {
+    alert(
+
+    );
+}
+
+function getBorrowedBook() {
+    alert(
+        "from my books in the top bar"
+    );
+}
+
+
+
+function EditOrDelete() {
+    alert(
+        `choose the book then click edit or delete`
+    );
+}
+
+// function does not completed yet
+// function addBook() {
+//     alert(
+//         1Click 
+//     )
+// }
