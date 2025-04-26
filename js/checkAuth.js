4b27d6a632578b6642ed0b14ed94239e7a068e61
mazenamr07 <11410120230308@stud.cu.edu.eg>
2025-04-26 18:26:26 +0300

    disabled guard for reCheck

--- js/checkAuth.js
+++ js/checkAuth.js
@@ -1,17 +1,23 @@
-function check(allowed) {
-  if (localStorage.getItem("loggedIn") == null) {
-    if (!allowed) {
-      window.location.replace("LogIn_SignUp page.html");
-    }
-  }
-}
+// function check(allowed) {
+//   if (localStorage.getItem("skip") != null) {
+//     return;
+//   }
+//   if (localStorage.getItem("loggedIn") == null) {
+//     if (!allowed) {
+//       window.location.replace("LogIn_SignUp page.html");
+//     }
+//   }
+// }
 
-function loginCheck() {
-  if (localStorage.getItem("loggedIn") != null) {
-    if (window.location.pathname.includes("html/")) {
-      window.location.replace("HomePage-user.html");
-    } else {
-      window.location.replace("html/HomePage-user.html");
-    }
-  }
-}
+// function loginCheck() {
+//   if (localStorage.getItem("skip") != null) {
+//     return;
+//   }
+//   if (localStorage.getItem("loggedIn") != null) {
+//     if (window.location.pathname.includes("html/")) {
+//       window.location.replace("HomePage-user.html");
+//     } else {
+//       window.location.replace("html/HomePage-user.html");
+//     }
+//   }
+// }
