26a017af817d02c60ccca643abcc2359b92394ec
mazenamr07 <11410120230308@stud.cu.edu.eg>
2025-04-27 05:04:30 +0300

    authCheck updated for admins

--- js/checkAuth.js
+++ js/checkAuth.js
@@ -1,7 +1,7 @@
+const userData = JSON.parse(localStorage.getItem("loggedIn"));
+
 function check(allowed, level) {
   if (localStorage.getItem("loggedIn")) {
-    const userData = JSON.parse(localStorage.getItem("loggedIn"));
-
     if (userData.authLevel >= level) {
       return;
     } else {
@@ -15,15 +15,15 @@ function check(allowed, level) {
 }
 
 function loginCheck() {
-  // if (localStorage.getItem("loggedIn")) {
-  //   if (JSON.parse(localStorage.getItem("loggedIn")).authLevel > 0) {
-  //     return;
-  //   } else {
-  //     if (window.location.pathname.includes("html/")) {
-  //       window.location.replace("HomePage-user.html");
-  //     } else {
-  //       window.location.replace("html/HomePage-user.html");
-  //     }
-  //   }
-  // }
+  if (localStorage.getItem("loggedIn")) {
+    if (userData.authLevel > 0) {
+      return;
+    } else {
+      if (window.location.pathname.includes("html/")) {
+        window.location.replace("HomePage-user.html");
+      } else {
+        window.location.replace("html/HomePage-user.html");
+      }
+    }
+  }
 }
