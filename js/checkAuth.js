2a5273bafcb0d5b9f8cbe7867a012a405c200fda
RahmaBahgat <162814707+RahmaBahgat@users.noreply.github.com>
2025-04-27 21:40:20 +0300

    Removing comments

--- js/checkAuth.js
+++ js/checkAuth.js
@@ -1,33 +1,33 @@
-// const userData = JSON.parse(localStorage.getItem("loggedIn"));
+const userData = JSON.parse(localStorage.getItem("loggedIn"));
 
-// function check(allowed, level) {
-//   if (localStorage.getItem("loggedIn")) {
+function check(allowed, level) {
+  if (localStorage.getItem("loggedIn")) {
 
-//     const userData = JSON.parse(localStorage.getItem("loggedIn"));
-//     // return userData.authLevel;
+    const userData = JSON.parse(localStorage.getItem("loggedIn"));
+    // return userData.authLevel;
 
-//     if (userData.authLevel >= level) {
-//       return;
-//     } else {
-//       window.location.replace("HomePage-user.html");
-//     }
-//   } else {
-//     if (!allowed) {
-//       window.location.replace("LogIn_SignUp page.html");
-//     }
-//   }
-// }
+    if (userData.authLevel >= level) {
+      return;
+    } else {
+      window.location.replace("HomePage-user.html");
+    }
+  } else {
+    if (!allowed) {
+      window.location.replace("LogIn_SignUp page.html");
+    }
+  }
+}
 
-// function loginCheck() {
-//   if (localStorage.getItem("loggedIn")) {
-//     if (userData.authLevel > 0) {
-//       return;
-//     } else {
-//       if (window.location.pathname.includes("html/")) {
-//         window.location.replace("HomePage-user.html");
-//       } else {
-//         window.location.replace("html/HomePage-user.html");
-//       }
-//     }
-//   }
-// }
+function loginCheck() {
+  if (localStorage.getItem("loggedIn")) {
+    if (userData.authLevel > 0) {
+      return;
+    } else {
+      if (window.location.pathname.includes("html/")) {
+        window.location.replace("HomePage-user.html");
+      } else {
+        window.location.replace("html/HomePage-user.html");
+      }
+    }
+  }
+}
