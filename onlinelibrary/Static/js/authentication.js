d668688c2cd82400291cce783d74d1ac2d719fff
Mariam Ehab <mariamehab135721@gmail.com>
2025-05-14 23:28:51 +0300

    fix the html

--- onlinelibrary/Static/js/authentication.js
+++ onlinelibrary/Static/js/authentication.js
@@ -1,206 +1,206 @@
-// Regex Constraints
-const nameRegex = /^[A-Za-z\s'-]{2,30}$/;
-const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d).{6,}$/;
+// // Regex Constraints
+// const nameRegex = /^[A-Za-z\s'-]{2,30}$/;
+// const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+// const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d).{6,}$/;
 
-const FnameInput = document.getElementById("first-name");
-FnameInput.addEventListener("blur", () => {
-  manageAlert(FnameInput, "Fname-alert", nameRegex);
-});
+// const FnameInput = document.getElementById("first-name");
+// FnameInput.addEventListener("blur", () => {
+//   manageAlert(FnameInput, "Fname-alert", nameRegex);
+// });
 
-const LnameInput = document.getElementById("last-name");
-LnameInput.addEventListener("blur", () => {
-  manageAlert(LnameInput, "Lname-alert", nameRegex);
-});
+// const LnameInput = document.getElementById("last-name");
+// LnameInput.addEventListener("blur", () => {
+//   manageAlert(LnameInput, "Lname-alert", nameRegex);
+// });
 
-const emailInput = document.getElementById("newEmail");
-emailInput.addEventListener("blur", () => {
-  manageAlert(emailInput, "email-alert", emailRegex);
-});
+// const emailInput = document.getElementById("newEmail");
+// emailInput.addEventListener("blur", () => {
+//   manageAlert(emailInput, "email-alert", emailRegex);
+// });
 
-const logInEmailInput = document.getElementById("email");
-logInEmailInput.addEventListener("blur", () => {
-  manageAlert(logInEmailInput, "login-email-alert", emailRegex);
-});
+// const logInEmailInput = document.getElementById("email");
+// logInEmailInput.addEventListener("blur", () => {
+//   manageAlert(logInEmailInput, "login-email-alert", emailRegex);
+// });
 
-const passwordInput = document.getElementById("signup-password");
-passwordInput.addEventListener("blur", () => {
-  manageAlert(passwordInput, "password-alert", passwordRegex);
-});
+// const passwordInput = document.getElementById("signup-password");
+// passwordInput.addEventListener("blur", () => {
+//   manageAlert(passwordInput, "password-alert", passwordRegex);
+// });
 
-const logInPasswordInput = document.getElementById("signin-password");
-logInPasswordInput.addEventListener("blur", () => {
-  manageAlert(logInPasswordInput, "login-password-alert", "empty");
-});
+// const logInPasswordInput = document.getElementById("signin-password");
+// logInPasswordInput.addEventListener("blur", () => {
+//   manageAlert(logInPasswordInput, "login-password-alert", "empty");
+// });
 
-const rePasswordInput = document.getElementById("confirm-password");
-rePasswordInput.addEventListener("blur", () => {
-  manageAlert(rePasswordInput, "rePassword-alert", "match");
-});
+// const rePasswordInput = document.getElementById("confirm-password");
+// rePasswordInput.addEventListener("blur", () => {
+//   manageAlert(rePasswordInput, "rePassword-alert", "match");
+// });
 
-let users = JSON.parse(localStorage.getItem("users")) || [];
-const createBtn = document.getElementById("create-btn");
-const loginBtn = document.getElementById("login-btn");
+// let users = JSON.parse(localStorage.getItem("users")) || [];
+// const createBtn = document.getElementById("create-btn");
+// const loginBtn = document.getElementById("login-btn");
 
-createBtn.addEventListener("click", () => {
-  if (
-    manageAlert(FnameInput, "Fname-alert", nameRegex) &
-    manageAlert(LnameInput, "Lname-alert", nameRegex) &
-    manageAlert(emailInput, "email-alert", emailRegex) &
-    manageAlert(passwordInput, "password-alert", passwordRegex) &
-    manageAlert(rePasswordInput, "rePassword-alert", "match")
-  ) {
-    if (checkEmail(emailInput)) {
-      console.log("Account Created");
-      signUp();
-    }
-  } else {
-    console.log("Not created");
-  }
-});
+// createBtn.addEventListener("click", () => {
+//   if (
+//     manageAlert(FnameInput, "Fname-alert", nameRegex) &
+//     manageAlert(LnameInput, "Lname-alert", nameRegex) &
+//     manageAlert(emailInput, "email-alert", emailRegex) &
+//     manageAlert(passwordInput, "password-alert", passwordRegex) &
+//     manageAlert(rePasswordInput, "rePassword-alert", "match")
+//   ) {
+//     if (checkEmail(emailInput)) {
+//       console.log("Account Created");
+//       signUp();
+//     }
+//   } else {
+//     console.log("Not created");
+//   }
+// });
 
-loginBtn.addEventListener("click", () => {
-  if (logInEmailInput.value == "admin" && logInPasswordInput.value == "admin") {
-    logIn(logInEmailInput.value, logInPasswordInput.value);
-  }
-  if (
-    manageAlert(logInEmailInput, "login-email-alert", emailRegex) &
-    manageAlert(logInPasswordInput, "login-password-alert", "empty")
-  ) {
-    if (checkPassword(logInEmailInput.value, logInPasswordInput.value)) {
-      logIn(logInEmailInput.value, logInPasswordInput.value);
-    }
-  }
-});
+// loginBtn.addEventListener("click", () => {
+//   if (logInEmailInput.value == "admin" && logInPasswordInput.value == "admin") {
+//     logIn(logInEmailInput.value, logInPasswordInput.value);
+//   }
+//   if (
+//     manageAlert(logInEmailInput, "login-email-alert", emailRegex) &
+//     manageAlert(logInPasswordInput, "login-password-alert", "empty")
+//   ) {
+//     if (checkPassword(logInEmailInput.value, logInPasswordInput.value)) {
+//       logIn(logInEmailInput.value, logInPasswordInput.value);
+//     }
+//   }
+// });
 
-// Functions
-function signUp() {
-  let firstName = document.getElementById("first-name").value;
-  let lastName = document.getElementById("last-name").value;
-  let email = document.getElementById("newEmail").value;
-  let password = document.getElementById("signup-password").value;
-  let isAdmin = document.getElementById("signup-option1").checked;
+// // Functions
+// function signUp() {
+//   let firstName = document.getElementById("first-name").value;
+//   let lastName = document.getElementById("last-name").value;
+//   let email = document.getElementById("newEmail").value;
+//   let password = document.getElementById("signup-password").value;
+//   let isAdmin = document.getElementById("signup-option1").checked;
 
-  let newUser = {
-    firstName,
-    lastName,
-    email,
-    password,
-    authLevel: isAdmin ? 1 : 0,
-    preferences: {
-      keepLogin: true,
-      profilePic: "",
-    },
-    books: {
-      borrowed: [],
-      favorite: [],
-    },
-  };
+//   let newUser = {
+//     firstName,
+//     lastName,
+//     email,
+//     password,
+//     authLevel: isAdmin ? 1 : 0,
+//     preferences: {
+//       keepLogin: true,
+//       profilePic: "",
+//     },
+//     books: {
+//       borrowed: [],
+//       favorite: [],
+//     },
+//   };
 
-  let users = JSON.parse(localStorage.getItem("users"));
-  users.push(newUser);
-  localStorage.setItem("users", JSON.stringify(users));
-  localStorage.setItem("loggedIn", JSON.stringify(newUser));
+//   let users = JSON.parse(localStorage.getItem("users"));
+//   users.push(newUser);
+//   localStorage.setItem("users", JSON.stringify(users));
+//   localStorage.setItem("loggedIn", JSON.stringify(newUser));
 
-  if (isAdmin) {
-    window.location.replace("HomePage-admin.html");
-  } else {
-    window.location.replace("HomePage-user.html");
-  }
-}
+//   if (isAdmin) {
+//     window.location.replace("HomePage-admin.html");
+//   } else {
+//     window.location.replace("HomePage-user.html");
+//   }
+// }
 
-function checkEmail(inputID) {
-  let users = JSON.parse(localStorage.getItem("users"));
+// function checkEmail(inputID) {
+//   let users = JSON.parse(localStorage.getItem("users"));
 
-  if (users.find((u) => u.email === inputID.value)) {
-    manageAlert(inputID, "email-alert", "emailFound");
+//   if (users.find((u) => u.email === inputID.value)) {
+//     manageAlert(inputID, "email-alert", "emailFound");
 
-    console.log("email found");
-    return;
-  }
-  console.log("email not found");
-  return true;
-}
+//     console.log("email found");
+//     return;
+//   }
+//   console.log("email not found");
+//   return true;
+// }
 
-function checkPassword(email, password) {
-  let users = JSON.parse(localStorage.getItem("users"));
-  const user = users.find((u) => u.email === email);
+// function checkPassword(email, password) {
+//   let users = JSON.parse(localStorage.getItem("users"));
+//   const user = users.find((u) => u.email === email);
 
-  if (user) {
-    if (user.password === password) {
-      console.log("Account verified");
-      return true;
-    } else {
-      console.log("Password incorrect");
-      manageAlert(logInPasswordInput, "login-password-alert", "loginMatch");
-      return false;
-    }
-  } else {
-    console.log("user doesn't exist");
-    manageAlert(logInEmailInput, "login-email-alert", "notUser");
-    return false;
-  }
-}
+//   if (user) {
+//     if (user.password === password) {
+//       console.log("Account verified");
+//       return true;
+//     } else {
+//       console.log("Password incorrect");
+//       manageAlert(logInPasswordInput, "login-password-alert", "loginMatch");
+//       return false;
+//     }
+//   } else {
+//     console.log("user doesn't exist");
+//     manageAlert(logInEmailInput, "login-email-alert", "notUser");
+//     return false;
+//   }
+// }
 
-function logIn(email) {
-  let users = JSON.parse(localStorage.getItem("users"));
-  const user = users.find((u) => u.email === email);
-  const keepLogin = document.getElementById("keep-login").checked;
+// function logIn(email) {
+//   let users = JSON.parse(localStorage.getItem("users"));
+//   const user = users.find((u) => u.email === email);
+//   const keepLogin = document.getElementById("keep-login").checked;
 
-  user.preferences.keepLogin = keepLogin;
+//   user.preferences.keepLogin = keepLogin;
 
-  localStorage.setItem("loggedIn", JSON.stringify(user));
+//   localStorage.setItem("loggedIn", JSON.stringify(user));
 
-  if (user.authLevel > 0) {
-    window.location.replace("HomePage-admin.html");
-  } else {
-    window.location.replace("HomePage-user.html");
-  }
-}
+//   if (user.authLevel > 0) {
+//     window.location.replace("HomePage-admin.html");
+//   } else {
+//     window.location.replace("HomePage-user.html");
+//   }
+// }
 
-function manageAlert(inputID, alertID, regex) {
-  const alert = document.getElementById(alertID);
-  let condition = null;
+// function manageAlert(inputID, alertID, regex) {
+//   const alert = document.getElementById(alertID);
+//   let condition = null;
 
-  if (regex == "match") {
-    condition = inputID.value != passwordInput.value;
-  } else if (regex == "emailFound" && inputID == emailInput) {
-    alert.innerHTML = "Email Already Exists";
-    condition = true;
-  } else if (regex == "loginMatch") {
-    alert.innerHTML = "Password is Incorrect";
-    condition = true;
-  } else if (regex == "notUser") {
-    alert.innerHTML = "User doesn't Exist";
-    condition = true;
-  } else if (regex == "empty") {
-    alert.innerHTML = "Field can't be Empty";
-    condition = !inputID.value;
-  } else {
-    if (inputID == emailInput) {
-      alert.innerHTML = "Email must consist of @ and . at the end";
-    }
-    condition = !regex.test(inputID.value);
-  }
+//   if (regex == "match") {
+//     condition = inputID.value != passwordInput.value;
+//   } else if (regex == "emailFound" && inputID == emailInput) {
+//     alert.innerHTML = "Email Already Exists";
+//     condition = true;
+//   } else if (regex == "loginMatch") {
+//     alert.innerHTML = "Password is Incorrect";
+//     condition = true;
+//   } else if (regex == "notUser") {
+//     alert.innerHTML = "User doesn't Exist";
+//     condition = true;
+//   } else if (regex == "empty") {
+//     alert.innerHTML = "Field can't be Empty";
+//     condition = !inputID.value;
+//   } else {
+//     if (inputID == emailInput) {
+//       alert.innerHTML = "Email must consist of @ and . at the end";
+//     }
+//     condition = !regex.test(inputID.value);
+//   }
 
-  if (condition) {
-    alert.classList.add("show");
-    alert.classList.remove("hide");
-  } else {
-    alert.classList.add("hide");
-    alert.classList.remove("show");
-    return true;
-  }
-}
+//   if (condition) {
+//     alert.classList.add("show");
+//     alert.classList.remove("hide");
+//   } else {
+//     alert.classList.add("hide");
+//     alert.classList.remove("show");
+//     return true;
+//   }
+// }
 
-function updateUser(userData, email) {
-  const users = JSON.parse(localStorage.getItem("users"));
-  let index = users.findIndex((u) => u.email === email);
+// function updateUser(userData, email) {
+//   const users = JSON.parse(localStorage.getItem("users"));
+//   let index = users.findIndex((u) => u.email === email);
 
-  if (index !== -1) {
-    users[index] = userData;
-    localStorage.setItem("users", JSON.stringify(users));
-  } else {
-    console.log("User not found.");
-  }
-}
+//   if (index !== -1) {
+//     users[index] = userData;
+//     localStorage.setItem("users", JSON.stringify(users));
+//   } else {
+//     console.log("User not found.");
+//   }
+// }
--- onlinelibrary/Static/js/checkAuth.js
+++ onlinelibrary/Static/js/checkAuth.js
@@ -1,54 +1,54 @@
-const userData = JSON.parse(localStorage.getItem("loggedIn"));
+// const userData = JSON.parse(localStorage.getItem("loggedIn"));
 
-if (localStorage.getItem("users") == null) {
-  let users = [
-    {
-      firstName: "admin",
-      lastName: "admin",
-      email: "admin",
-      password: "admin",
-      authLevel: "1",
-      preferences: {
-        keepLogin: true,
-        profilePic: "",
-      },
-      books: {
-        borrowed: [],
-        favorite: [],
-      },
-    },
-  ];
-  localStorage.setItem("users", JSON.stringify(users));
-  console.log("(users) array created");
-}
+// if (localStorage.getItem("users") == null) {
+//   let users = [
+//     {
+//       firstName: "admin",
+//       lastName: "admin",
+//       email: "admin",
+//       password: "admin",
+//       authLevel: "1",
+//       preferences: {
+//         keepLogin: true,
+//         profilePic: "",
+//       },
+//       books: {
+//         borrowed: [],
+//         favorite: [],
+//       },
+//     },
+//   ];
+//   localStorage.setItem("users", JSON.stringify(users));
+//   console.log("(users) array created");
+// }
 
-function check(allowed, level) {
-  if (localStorage.getItem("loggedIn")) {
-    const userData = JSON.parse(localStorage.getItem("loggedIn"));
-    // return userData.authLevel;
+// function check(allowed, level) {
+//   if (localStorage.getItem("loggedIn")) {
+//     const userData = JSON.parse(localStorage.getItem("loggedIn"));
+//     // return userData.authLevel;
 
-    if (userData.authLevel >= level) {
-      return;
-    } else {
-      window.location.replace("HomePage-user.html");
-    }
-  } else {
-    if (!allowed) {
-      window.location.replace("LogIn_SignUp page.html");
-    }
-  }
-}
+//     if (userData.authLevel >= level) {
+//       return;
+//     } else {
+//       window.location.replace("HomePage-user.html");
+//     }
+//   } else {
+//     if (!allowed) {
+//       window.location.replace("LogIn_SignUp page.html");
+//     }
+//   }
+// }
 
-function loginCheck() {
-  if (localStorage.getItem("loggedIn")) {
-    if (userData.authLevel > 0) {
-      return;
-    } else {
-      if (window.location.pathname.includes("html/")) {
-        window.location.replace("HomePage-user.html");
-      } else {
-        window.location.replace("html/HomePage-user.html");
-      }
-    }
-  }
-}
+// function loginCheck() {
+//   if (localStorage.getItem("loggedIn")) {
+//     if (userData.authLevel > 0) {
+//       return;
+//     } else {
+//       if (window.location.pathname.includes("html/")) {
+//         window.location.replace("HomePage-user.html");
+//       } else {
+//         window.location.replace("html/HomePage-user.html");
+//       }
+//     }
+//   }
+// }
