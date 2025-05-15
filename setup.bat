01c5dbe2982449105f45d1da4a52d929a6a42010
Mariam Ehab <mariamehab135721@gmail.com>
2025-05-16 01:11:15 +0300

    ya rab a5er push

--- setup.bat
+++ setup.bat
@@ -1,17 +1,30 @@
 @echo off
 echo Setting up your development environment...
 
-:: Create and activate virtual environment
-python -m venv venv
+:: Check if requirements.txt exists, if not create it
+if not exist requirements.txt (
+    echo Creating requirements.txt...
+    pip freeze > requirements.txt
+)
+
+:: Check if venv already exists
+if not exist venv (
+    echo Creating virtual environment...
+    python -m venv venv
+)
+
+:: Activate virtual environment
 call venv\Scripts\activate
 
 :: Install requirements
+echo Installing requirements...
 pip install -r requirements.txt
 
 :: Navigate to project directory
 cd onlinelibrary
 
 :: Run migrations
+echo Running migrations...
 python manage.py makemigrations
 python manage.py migrate
 
